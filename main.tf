b577054ccc4f3e42f76050a7a3444ef7859a7e2d
Diego OJ <diegoaojeda@gmail.com>
2021-08-26 10:13:44 -0300

    Add missing public access restrictions

--- main.tf
+++ main.tf
@@ -48,6 +48,14 @@ resource "aws_s3_bucket" "this" {
   tags = var.tags
 }
 
+resource "aws_s3_bucket_public_access_block" "default" {
+  bucket                  = aws_s3_bucket.this.id
+  block_public_acls       = true
+  ignore_public_acls      = true
+  block_public_policy     = true
+  restrict_public_buckets = true
+}
+
 resource "aws_s3_bucket_policy" "default" {
   bucket = aws_s3_bucket.this.id
   policy = <<POLICY
