f03ccdd98692d78903f3c8f64bb9c30a2eee68a0
David Reed <david.reedfrancis@sainsburys.co.uk>
2022-04-20 10:46:34 +0100

    Adds Support for AWS Provider 4.9+
    This version works fine with the current S3 setup.

--- versions.tf
+++ versions.tf
@@ -3,6 +3,6 @@ terraform {
   required_version = ">= 0.12.28"
 
   required_providers {
-    aws = "~> 3.0"
+    aws = ">= 3.0, != 4.0, != 4.1, != 4.2, != 4.3, != 4.4, != 4.5, != 4.6, != 4.7, != 4.8"
   }
 }
